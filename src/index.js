261fe0be33ebde9f51e1780bde9f2a89d8b026f5
AllyssonLPereira <allyssonlunguinho.03i@gmail.com>
2025-04-02 16:56:40 -0300

    criando a funcao que verifica as palavras repetidas

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ const filePath = process.argv;
 const link = filePath[2];
 
 fs.readFile(link, "utf-8", (err, text) => {
-    console.log(text);
-})
+    CheckDuplicateWords(text);
+});
+
+// criar um array com as palavras
+// contar as ocorrências
+// montar um objeto com o resultado
+
+function CheckDuplicateWords(text) {
+    const listWords = text.split(" ");
+    const result = {};
+
+    listWords.forEach(word => {
+        result[word] = (result[word] || 0) + 1
+    });
+
+    console.log(result)
+};
