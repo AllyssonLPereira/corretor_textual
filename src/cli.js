245580aa29b2cdf5b9c82777ed6b1a4dfab1588a
AllyssonLPereira <allyssonlunguinho.03i@gmail.com>
2025-04-09 16:28:58 -0300

    deploy do projeto

--- src/cli.js
+++ src/cli.js
@@ -1,33 +1,61 @@
 import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+import { Command } from "commander";
 import { accountWords } from "./index.js";
 import { mountFileOutput } from "./helpers.js";
 import treatErrors from "./errors/functionsErrors.js";
 
-const pathOrigin = process.argv;
-const link = pathOrigin[2];
+const program = new Command();
 
-const pathDestination = pathOrigin[3];
+program
+    .version("0.0.1")
+    .option("-t, --text <string>", "caminho do texto a ser processado")
+    .option("-d, --destination", "caminho da pasta onde salvar o arquivo resultante do processamento")
+    .action((options) => {
+        const { text, destination } = options;
 
-fs.readFile(link, "utf-8", (err, text) => {
-    try {
-        if (err) throw err;
-        const result = accountWords(text);
-        writeFile(result, pathDestination);
+        if (!text || !destination) {
+            console.error(chalk.red("error: favor inserir caminho de origem e destino"));
+            program.help();
+            return;
+        }
 
-    } catch(err) {
-        treatErrors(err)
-    }
-});
+        const textPath = path.resolve(text);
+        const destinationPath = path.resolve(destination);
 
-async function writeFile(listWords, path) {
-    const file = `${path}/resultado.txt`;
+        try {
+            processFile(textPath, destinationPath);
+            console.log(chalk.green("texto processado com sucesso"))
+        } catch (err) {
+            console.error(chalk.red("ocorreu um erro no processamento"), err)
+        }
+    })
+
+program.parse();
+
+function processFile(text, destination) {
+    fs.readFile(text, "utf-8", (err, text) => {
+        try {
+            if (err) throw err;
+            const result = accountWords(text);
+            writeFile(result, destination);
+
+        } catch (err) {
+            treatErrors(err);
+        }
+    });
+}
+
+async function writeFile(listWords, destination) {
+    const file = `${destination}/resultado.txt`;
     const text = mountFileOutput(listWords);
 
     try {
         await fs.promises.writeFile(file, text);
         console.log("Arquivo criado");
 
-    } catch(err) {
+    } catch (err) {
         throw err;
     }
 }
